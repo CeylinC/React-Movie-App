d3de210682e79265e8258cd5d17a5d94e62a0c82
Ceylin <caltepeceylin@gmail.com>
2023-09-14 22:46:33 +0300

    fix: remove searchbox from favorite movies page

--- movie-app/src/page/home/home/Homepage.tsx
+++ movie-app/src/page/home/home/Homepage.tsx
@@ -2,16 +2,16 @@ import './Homepage.css';
 import {Navbar} from '../../../components';
 import { Outlet, useSearchParams } from 'react-router-dom';
 import { useUserControl, useUserStore } from '../../../hook';
+import { useEffect, useState } from 'react';
 
 export function Homepage() {
-  const [searchParams, setSearchParams] = useSearchParams();
   const {user, setUser} = useUserStore();
   
   useUserControl(user, setUser);
   
   return (
     <div className="Homepage">
-      <Navbar setSearchParam={setSearchParams} username={user ? user.username : undefined}/>
+      <Navbar username={user ? user.username : undefined}/>
       <Outlet/>
     </div>
   );
